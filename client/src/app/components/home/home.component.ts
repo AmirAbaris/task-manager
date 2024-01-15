d5d2ccc2d5e72caae9a2f0c955dfcb1e668d3b81
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-01-15 21:48:37 +0330

    imported module

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatInputModule],
+  imports: [MatInputModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
